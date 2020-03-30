d90fea7ee4262358dbcad61259b2c2a4a69a306f
cnatzke <cnatzke@triumf.ca>
2020-03-30 10:43:14 -0700

    Add header for treeConvert

--- src/treeConverter.cpp
+++ src/treeConverter.cpp
@@ -7,6 +7,7 @@
 #include "TParserLibrary.h" // needed for GetRunNumber
 //#include "TGRSIRunInfo.h"
 #include "progress_bar.h"
+#include "treeConverter.h"
 
 #include <iostream>
 #include <iomanip> // setiosflags
@@ -15,47 +16,6 @@
 #ifndef __CINT__
 #include "TGriffin.h"
 #endif
-
-// Global things
-
-//TGRSIRunInfo *info = NULL;
-TPPG *ppg          = NULL;
-TChain *gChain     = NULL;
-TList *outList      = NULL;
-
-std::vector<std::string> RootFiles;
-std::vector<std::string> CalFiles;
-std::vector<std::string> InfoFiles;
-
-class Notifier : public TObject {
-   public:
-      Notifier() { }
-      ~Notifier() { }
-
-      void AddChain(TChain *chain)       { fChain = chain; }
-      void AddRootFile(std::string name) { RootFiles.push_back(name); }
-      void AddInfoFile(std::string name) { InfoFiles.push_back(name); }
-      void AddCalFile(std::string name)  { CalFiles.push_back(name); }
-
-      bool Notify() {
-         printf("%s loaded.\n", fChain->GetCurrentFile()->GetName());
-         ppg = (TPPG*)fChain->GetCurrentFile()->Get("TPPG");
-
-         if (CalFiles.size() > 0){
-            TChannel::ReadCalFile(CalFiles.at(0).c_str());
-         } else {
-             std::cout << "No calibration file loaded." << std::endl;
-         }
-
-         return true;
-      } // end Notify
-
-   private:
-      TChain *fChain;
-      std::vector<std::string> RootFiles;
-      std::vector<std::string> CalFiles;
-      std::vector<std::string> InfoFiles;
-}; // End Notifier class
 
 Notifier *notifier = new Notifier;
 
